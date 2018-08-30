10d03152d0d4005ab8b0cadcdf5dca05b4c214f1
CouldI <sym0222@naver.com>
2018-08-30 21:55:34 +0900

    8/30 학습

---
+++ webdeveloper/js basic/whileloop.js
@@ -0,0 +1,35 @@
+console.log("PRINTING ALL NUMBERS BETWEEN -10 AND 19")
+var count = -10;
+
+while (count <= 19) {
+	console.log(count);
+	count++;
+}
+
+console.log("PRINTING ALL EVEN BETWEEN 10 AND 40")
+var count = 10;
+
+while (count <= 40) {
+	console.log(count);
+	count += 2;
+}
+
+console.log("PRINTING ALL EVEN BETWEEN 10 AND 40")
+var count = 300;
+
+while (count <= 333) {
+	if (count % 2 === 1) {
+		console.log(count);	
+	}
+	count++;
+}
+
+console.log("PRINTING ALL EVEN BETWEEN 10 AND 40")
+var count = 5;
+
+while (count <= 50) {
+	if (count % 5 === 0 && count % 3 === 0) {
+		console.log(count);	
+	}
+	count++;
+}
